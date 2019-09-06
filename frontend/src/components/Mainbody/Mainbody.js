90e1bd8452bc7103cb0480df35d90295c12cdcac
adamc66 <adam.cote66@gmail.com>
2019-09-06 15:41:28 -0400

    getting closer to connecting React and Django

---
+++ frontend/src/components/Mainbody/Mainbody.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mainbody() {
+    return (
+        <div>
+            <h1>THIS IS A TEST</h1>
+        </div>
+    )
+}
+
+export default Mainbody
--- frontend/src/pages/app/App.js
+++ frontend/src/pages/app/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from '../../components/Header/Header'
-
+import Mainbody from '../../components/Mainbody/Mainbody'
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Mainbody />
     </div>
   );
 }
