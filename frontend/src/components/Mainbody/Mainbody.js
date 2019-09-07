4e55c7fcdd4cfa5d624e4d114f98799706e809ce
acote66 <adam.cote66@gmail.com>
2019-09-07 13:53:25 -0400

    got it. had to use state..

--- frontend/src/components/Mainbody/Mainbody.js
+++ frontend/src/components/Mainbody/Mainbody.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios';
 
 function Mainbody() {
+    const [data, setData] = useState("loading")
 
-    let theresponse="hello"
 
     const GET_VERB = () => {
         axios.get("/api/it/", {})
           .then((response) => {
               console.log("__GET VERB CALLED")
-              theresponse = response.data.it
-              console.log(theresponse)
+              setData(response.data.it)
+              console.log(data)
             }).catch((e) => console.log("Error:", e))
         }
     GET_VERB()
-    console.log("__AFTER GET VERB IS CALLED",theresponse)
+    console.log("__AFTER GET VERB IS CALLED",data)
     return (
         
         <div>
             <h1>THIS IS A TEST</h1>
-            <h3>{theresponse}</h3>
+            <h3>{data}</h3>
         </div>
     )
 
