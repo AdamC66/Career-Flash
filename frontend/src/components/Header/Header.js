5973ec717a3c736446d33b57aa7ab94d754ff522
adamc66 <adam.cote66@gmail.com>
2019-09-13 11:43:27 -0400

    added dropdown menu to header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -37,12 +37,17 @@ function Header() {
         <li className="nav-item">
         <h4><a className="nav-link" href="/">About</a></h4>
         </li>
-        <li className="nav-item">
-        <h4><a className="nav-link" href="/tracker">Tracker</a></h4>
-        </li>
         </ul>
         <h2 className="nav-item"><a className="nav-link" href="/tracker"><FontAwesomeIcon icon={faBell}  color="rgba(255,255,255,0.5)" /></a></h2>
-        <h2 className="nav-item"><a className="nav-link" href="/profile"><FontAwesomeIcon icon={faUser} color="rgba(255,255,255,0.5)"/> {userName}</a></h2>
+        <h2 className="nav-item dropdown" role='button'><a href="#" className="nav-link dropdown-toggle" role="button" data-toggle="dropdown"><FontAwesomeIcon icon={faUser} color="rgba(255,255,255,0.5)"/> {userName}</a>
+        <div className="dropdown-menu" x-placement="bottom-start" style={{position: 'absolute', transform: 'translate3d(0px, 39px, 0px)', top: '0px', left: '0px', willChange: 'transform'}}>
+        <h5><a className="dropdown-item" href="/profile">Profile</a></h5>
+        <h5><a className="dropdown-item" href="/tracker">Tracker</a>></h5>
+        <div className="dropdown-divider"></div>
+        <h5><a className="dropdown-item" href="#">Logout</a></h5>
+      </div>
+      </h2>
+
     </div>
    
     </nav>
