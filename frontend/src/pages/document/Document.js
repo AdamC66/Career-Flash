89c53230c86c8e88eb076460643dbbd5075916ef
adamc66 <adam.cote66@gmail.com>
2019-09-16 16:29:45 -0400

    things and stuff

--- frontend/src/pages/document/Document.js
+++ frontend/src/pages/document/Document.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import MyDocument from '../../components/MyDocument/MyDocument';
 import CommentsBar from '../../components/CommentsBar/CommentsBar';
 import './document.css'
-
+import main_url from '../../config';
 export default function Document() {
     const [comments, setComments] = useState([
         {
@@ -15,11 +15,47 @@ export default function Document() {
         comment_time:"15-09-2018",
         user:"admin",
         profile:"is this needed?"},])
+    const [documentType, setDocumentType]= useState('resume')
+    const [resumeLink, setResumeLink] = useState ("test.pdf")
+    const [coverLink, setCoverLink] = useState ("test2.pdf")
 
+    const checkLogin = () => {
+        const userToken = window.localStorage['token']
+        if(userToken !== 'null'){
+            main_url.get('/api/profiles/', {
+                headers: {
+                    Authorization: `Token ${userToken}` 
+                }
+            })
+            .then(res => {
+                if (res.data[0].resume){
+                    setResumeLink(res.data[0].resume.substr(50));
+                }
+                if (res.data[0].cover_letter){
+                    setCoverLink(res.data[0].cover_letter.substr(50));
+                }
+                console.log(res.data)
+            });
+        }}
+    let myDoc = <MyDocument path={documentType == "resume" ? resumeLink : coverLink} width={600}/>
+
+    const handleChange = (doc) =>{
+        setDocumentType(doc)
+        if (doc == "coverLetter"){
+            console.log(doc)
+            myDoc = <MyDocument path={coverLink} width={600}/>
+        }else{
+            console.log(doc)
+            myDoc = <MyDocument path={resumeLink} width={600}/>
+        }
+    }
     return (
         <div className="document-main-container">
+        
             <div className="document-container">
-                <MyDocument path="test.pdf" width={600}/>
+            <button className="btn btn-primary" value="resume" onClick={()=> handleChange('resume')}>Resume</button>
+            <button className="btn btn-primary" value="coverletter" onClick={()=> handleChange('coverLetter')} >CoverLetter</button>
+            { myDoc }
             </div>
             <div className="comment-container">
                 <CommentsBar comments={comments}/>
