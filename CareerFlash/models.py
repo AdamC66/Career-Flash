0b673003f1a4da3c15fac4553c48763634b5e4de
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-16 11:31:12 -0400

    made small changes to path for filefields. added blank is true for application and profile model field

--- CareerFlash/models.py
+++ CareerFlash/models.py
@@ -20,25 +20,25 @@ class Application(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="job_applications")
     company = models.CharField(max_length=255)
     position = models.CharField(max_length=255)
-    description = models.TextField(null=True)
-    notes = models.TextField(null=True)
-    contact_name = models.CharField(max_length=255, null=True)
-    contact_email = models.CharField(max_length=255, null=True)
-    contact_phone = PhoneField(blank=True, help_text='Contact phone number')
+    description = models.TextField(blank=True, null=True,)
+    notes = models.TextField(blank=True, null=True,)
+    contact_name = models.CharField(max_length=255, blank=True, null=True)
+    contact_email = models.CharField(max_length=255, blank=True, null=True)
+    contact_phone = PhoneField(blank=True, null=True, help_text='Contact phone number')
     date_submitted = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, null=True)
+    status = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return f"{self.company} {self.position} submitted on {self.date_submitted}"
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles_user')
-    brand_statement = models.TextField(null=True, max_length=500)
-    resume = models.FileField(null=True, upload_to='/frontend/public/tempdocs/resumes')
-    cover_letter = models.FileField(null=True, upload_to='/frontend/public/tempdocs/coverletters')
-    github = models.URLField(null=True, validators=[URLValidator])
-    linkedin = models.URLField(null=True, validators=[URLValidator])
-    portfolio = models.URLField(null=True, validators=[URLValidator])
+    brand_statement = models.TextField(blank=True, null=True, max_length=500)
+    resume = models.FileField(blank=True, null=True, upload_to='frontend/public/tempdocs/resumes/')
+    cover_letter = models.FileField(blank=True, null=True, upload_to='frontend/public/tempdocs/coverletters/')
+    github = models.URLField(blank=True, null=True, validators=[URLValidator])
+    linkedin = models.URLField(blank=True, null=True, validators=[URLValidator])
+    portfolio = models.URLField(blank=True, null=True, validators=[URLValidator])
 
     def __str__(self):
         return f"{self.brand_statement}"
