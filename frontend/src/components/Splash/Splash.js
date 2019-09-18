f832ef544741d4cf77f83ad656c054f6bc5b7cd4
Stanley Sham <stanley.sham94@gmail.com>
2019-09-18 12:24:25 -0400

    updated main page to be dynamic

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import splash from './img/splash.jpg'
 import "./splash.css"
 import { Link } from 'react-router-dom';
 function Splash() {
+    const [loggedInLinks, setLoggedInLinks] = useState(
+    <>
+        <Link to = '/join'>
+            <button className="btn btn-primary" value="join" style={{marginRight: '5px'}}>Join Now</button>
+        </Link>
+        <Link to = '/login'>
+            <button className="btn btn-primary" value="login" style={{marginLeft: '5px'}}>Login</button>
+        </Link>
+    </>);
+
+    const checkLogin = () => {
+        const userToken = window.localStorage['token'];
+        if (userToken !== null){
+            setLoggedInLinks(
+                <Link to = '/dashboard'>
+                    <button className="btn btn-primary" value="join" style={{marginRight: '5px'}}>Dashboard</button>
+                </Link>
+            )
+        } 
+    }
+
+    window.addEventListener('load', checkLogin)
+
+
     return (
         <div>
             <div className = 'splashimg'>
                 <div className='about'>
                 <h1>Welcome to Career Flash</h1>
                 <h4>Your Job Search Control Center</h4>
-                <Link to = '/join'>
-                <button className="btn btn-primary" value="join" style={{marginRight: '5px'}}>Join Now</button>
-                </Link>
-                <Link to = '/login'>
-                <button className="btn btn-primary" value="login" style={{marginLeft: '5px'}}>Login</button>
-                </Link>
+                {loggedInLinks}
                 </div>
                 <img src={splash} alt=""/>
-                
             </div>
         </div>
     )
