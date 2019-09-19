770a98ddd2901b9266e6ad737066694d8edfc569
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-09-19 11:13:00 -0400

    changed about text and did some styling for it

--- frontend/src/pages/about/About.js
+++ frontend/src/pages/about/About.js
@@ -6,11 +6,11 @@ function About() {
         <div>
             <div className = 'splashimg'>
                 <div className='about'>
-                <h1>Our Story</h1>
-                <h5>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man.</h5>
+                <h1 style={{color: "white", textShadow: "0px 0px 8px black"}}>Our Story</h1>
+                <h5 style={{color: "white", textShadow: "0px 0px 8px black"}}>CareerFlash's goal is to be the homebase for your job hunt. From experience we understand how arduous a task looking for a new job can be, and we wanted to simplify the process as much as possible. Our core tools provides you a sleek and user friendly interface, to keep track of all your applications in one spot and filter them based on the status. Another key feature is our document upload page, which will hold versions of your resume and cover letter.</h5>
                 <br/>
-                <h5>
-                The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.</h5>
+                <h5 style={{color: "white", textShadow: "0px 0px 8px black"}}>
+                For Organizations, we equip you with a dashboard to view all your associated users and keep track of their job hunting progresss. Organization admins can also leave comments on user resume's and cover letter's that support a synergistic experience between everyone. </h5>
                 </div>
                 <img src={splash} alt=""/>       
             </div>
